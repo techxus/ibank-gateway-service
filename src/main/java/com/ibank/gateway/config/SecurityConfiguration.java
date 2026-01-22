99678359e31adf7381a4a5016f2c2335abb2feda
softnet <daniel@softnet.cloud>
2026-01-21 19:05:09 -0500

    Fixes

---
+++ src/main/java/com/ibank/gateway/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.ibank.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .authorizeExchange(ex -> ex
+                    .pathMatchers(
+                            "/actuator/health/**",
+                            "/api/v1/user/actuator/health/**",
+                            "/api/v1/notification/actuator/health/**"
+                    ).permitAll()
+                    .anyExchange().permitAll()   // for now (we’ll lock down later)
+            )
+            .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+            .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+            .build();
+    }
+}
